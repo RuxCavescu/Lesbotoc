b63c6f5bbe4d0748157f48c218f3f6a3d8af6f49
RuxCavescu <rux.cavescu@gmail.com>
2022-07-12 10:28:13 +0200

    styling - added stylesheet for contacts

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -7,21 +7,39 @@
             {{ Session::get('success_message') }}
         </div>
     @endif
-<table>
-    <tr>
-        <td>Name</td>
-        <td>Email</td>
-        <td>Phone Number</td>
-    </tr>
-  @foreach ($contacts as $contact)
-    <tr>
-        <td>{{$contact->name}} </td>
-        <td>{{$contact->email}} </td>
-        <td>{{$contact->phone}} </td>
-        <td><button>X</button></td>
-        <td><button><a href="contacts/{{$contact->id}}/edit">Edit</a></button></td>
-    </tr>
-  @endforeach
-</table>
+
+    <div class="contacts">
+        @foreach ($contacts as $contact)
     
+            <div class="contacts__card">
+                <h3>{{$contact->name}}</h3>
+                <div>Email: {{$contact->email}}</div>
+                <div>Phone Number: {{$contact->phone ? $contact->phone : '-'}}</div>
+                <div class="buttons">
+                    <a href="#"><i class="fa fa-trash"></i></a>
+                    <a href="contacts/{{$contact->id}}/edit"><i class="fa fa-edit"></i></a>
+                </div>
+            </div>
+    
+        @endforeach
+    </div>
+
+    
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone Number</td>
+        </tr>
+    @foreach ($contacts as $contact)
+        <tr>
+            <td>{{$contact->name}} </td>
+            <td>{{$contact->email}} </td>
+            <td>{{$contact->phone}} </td>
+            <td><i class="fa fa-trash"></i>Edit</td>
+            <td><a href="contacts/{{$contact->id}}/edit"><i class="fa fa-edit"></a></td>
+        </tr>
+    @endforeach
+    </table>
+        
 @endsection
