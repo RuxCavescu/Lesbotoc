3ff578d11160b9a844c15000c7aa3aed4d912701
RuxCavescu <rux.cavescu@gmail.com>
2022-07-14 16:45:33 +0200

    delete image

--- resources/views/images/show-all-images.blade.php
+++ resources/views/images/show-all-images.blade.php
@@ -2,11 +2,20 @@
     <h2>View and add images</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea provident expedita vitae error atque nobis iste tempore cumque unde placeat dolorem et, aspernatur eos repudiandae, fugit alias facere laudantium ad.</p>
 
+    {{-- @include('layout/messages') --}}
+
     <div class="images__container">
         @foreach ($images as $image)
             <div class="image">
                 <h4>{{$image->alt}}</h4>
+                <form action="{{ route('delete-image', ['id' => $image->id])}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button class="delete__image-btn">Delete</button>
+                </form>
                 <img src="{{$image->path}}" alt="">
+
             </div>
+
         @endforeach
     </div>
